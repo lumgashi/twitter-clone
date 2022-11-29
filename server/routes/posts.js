e114f453b5369e8a6838694574f669669200ad09
Lum Gashi <lumgashi97@hotmail.com>
2022-11-29 17:16:49 +0100

    frontend/backend
    Added new files

---
+++ server/routes/posts.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import {verifyToken} from '../middleware/auth.js';
+import { getFeedPosts, getUserPosts,likePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+// read
+router.get('/',verifyToken, getFeedPosts);
+router.get('/:userId/posts',verifyToken, getUserPosts);
+
+//patch
+router.get('/:id/like',verifyToken,likePost)
+
+
+export default router;
+
+
+
+
